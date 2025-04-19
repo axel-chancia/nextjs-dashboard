7afcec44b48d19287660d622af41629cfb5643aa
Makita Arsel <amakita124@gmail.com>
2025-04-19 14:50:36 +0100

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Customers  from "@/app/dashboard/customers/page";
+import Invoices  from "@/app/dashboard/invoices/page";
+
+export default function page() {
+  return (
+    <>
+      <div>    
+       page dashboard
+    </div>
+    </>
+  )
+}
